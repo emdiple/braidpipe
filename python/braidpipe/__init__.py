a735bc97d7f8d413d3f09a16804573cd36848943
emdiple <emdiple.95@gmail.com>
2026-09-03 09:44:50 +0330

    chore(release): braidpipe 0.4.0

--- python/braidpipe/__init__.py
+++ python/braidpipe/__init__.py
@@ -28,7 +28,7 @@ from .remote import RemoteWorkerLink, connect
 from .runner import FrameContext, run, worker
 from .shm import SharedMemoryManager, attach
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "BackgroundModel",
